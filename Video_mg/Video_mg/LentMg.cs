81c65059eb4db7fbde57885461b2ca4466ff1a61
jump8779 <jump154159@gmail.com>
2019-09-16 20:40:27 +0900

    LentMg 폼 기능 구현 (2)
    + 등록 버튼 기능 구현
    + 저장 버튼 기능 구현
    + 비디오 회수 버튼 기능 구현

--- Video_mg/Video_mg/LentMg.cs
+++ Video_mg/Video_mg/LentMg.cs
@@ -32,23 +32,23 @@ namespace Video_mg
         {
             String c = "server=jump8779;database=Videomg;uid=sa;pwd=std001";
             sql = new SqlConnection(c);
-            sql.Open();
+            sql.Open(); // 서버 연결
         }
         public void MemberSelect()
         {
-            String s = "SELECT * FROM lent WHERE mcode=@mcode";
+            String s = "SELECT * FROM lent WHERE mcode=@mcode"; 
             sqlC2 = new SqlCommand(s, sql);
             sqlC2.Parameters.Add("@mcode", SqlDbType.Int, 4);
             sqlC2.Parameters["@mcode"].Value = TBcode.Text;
 
             sqlA2 = new SqlDataAdapter();
-            sqlA2.SelectCommand = sqlC2;
+            sqlA2.SelectCommand = sqlC2; // Select문 명령으로 sqlC2를 넘겨줌
             try
             {
                 DS2 = new DataSet();
                 DS2.Clear();
-                sqlA2.TableMappings.Add("Table", "lent");
-                sqlA2.Fill(DS2, "Table");
+                sqlA2.TableMappings.Add("Table", "lent"); // 소스네임"Table"의 "lent"테이블 생성
+                sqlA2.Fill(DS2, "Table"); // Fill의 파라미터로는 소스네임을 넘겨줌
 
                 lbl_alllentcount.Text = DS2.Tables["lent"].Rows.Count + " 개";
                 dgVideo.DataSource = DS2.Tables["lent"];
@@ -85,17 +85,16 @@ namespace Video_mg
                 sqlA.Fill(DS, "Table");
                 if (DS.Tables["membermg"].Rows.Count == 1)
                 {
-                    foreach (DataRow Row in DS.Tables["membermg"].Rows)
-                    {
-                        TBcode.Text = Row["mcode"].ToString();
-                        TBname.Text = Row["iname"].ToString();
-                        TBjumin.Text = Row["jumin"].ToString();
-                        TBsinbun.Text = Row["sinbun"].ToString();
-                        TBphone.Text = Row["phone"].ToString();
-                        TBpcs.Text = Row["pcs"].ToString();
-                        TBzip.Text = Row["zip"].ToString();
-                        TBaddress.Text = Row["_address"].ToString();
-                    }
+                    DataRow Row = DS.Tables["membermg"].Rows[0];
+                    TBcode.Text = Row["mcode"].ToString();
+                    TBname.Text = Row["iname"].ToString();
+                    TBjumin.Text = Row["jumin"].ToString();
+                    TBsinbun.Text = Row["sinbun"].ToString();
+                    TBphone.Text = Row["phone"].ToString();
+                    TBpcs.Text = Row["pcs"].ToString();
+                    TBzip.Text = Row["zip"].ToString();
+                    TBaddress.Text = Row["_address"].ToString();
+
                     MemberSelect();
                 }
                 else
@@ -148,17 +147,181 @@ namespace Video_mg
 
         private void Bt_vmatch_Click(object sender, EventArgs e)
         {
-            
+            bt_save.Enabled = true;
+
+            SqlCommand sqlC3;
+            SqlDataAdapter sqlA3 = new SqlDataAdapter();
+            DataSet DS3 = new DataSet();
+
+            string s = "SELECT * FROM videomg WHERE vcode='" + TBvcode.Text +
+                "' OR _subject='" + TBvtitle.Text + "'";
+            sqlC3 = new SqlCommand(s, sql);
+            sqlA3.SelectCommand = sqlC3;
+
+            SqlCommand sqlC4;
+            SqlDataAdapter sqlA4 = new SqlDataAdapter();
+            string s2 = "SELECT * FROM lentoption";
+
+            sqlC4 = new SqlCommand(s2, sql);
+            sqlA4.SelectCommand = sqlC4;
+
+            try
+            {
+                sqlA3.TableMappings.Add("Table", "videomg");
+                sqlA3.Fill(DS3, "Table");
+                sqlA4.TableMappings.Add("Table", "lentoption");
+                sqlA4.Fill(DS3, "Table");
+
+                DataRow VRow, Row;
+                VRow = DS3.Tables["videomg"].Rows[0];
+                TBvcode.Text = VRow["vcode"].ToString();
+                TBvtitle.Text = VRow["_subject"].ToString();
+                DateTime dt = dateTimePicker1.Value;
+
+                foreach (DataRow ORow in DS3.Tables["lentoption"].Rows)
+                {
+                    DateTime date = Convert.ToDateTime(VRow["out_date"]);
+                    date = date.AddDays(Convert.ToDouble(ORow["vdate"]));
+                    if (DateTime.Today.Date < date)
+                    {
+                        Row = DS3.Tables["lentoption"].Rows[0];
+                    }
+                    else
+                    {
+                        Row = DS3.Tables["lentoption"].Rows[1];
+                    }
+                    TBlentpay.Text = Row["lent_pay"].ToString();
+                    TBdelaypay.Text = Row["delay_pay"].ToString();
+                    TBoutdate.Text = dt.AddDays(Convert.ToDouble(Row["lent_date"])).ToString();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void Bt_save_Click(object sender, EventArgs e)
         {
-            
+            SqlCommand sqlC3;
+            string s = "INSERT INTO lent (mcode, vcode, lent_date, return_date1," +
+                " return_date2, lent_pay, delay_pay1, delay_pay2, vsubject) " +
+                "VALUES (@mcode, @vcode, @lent_date, @return_date1, @return_date2, " +
+                "@lent_pay, @delay_pay1, @delay_pay2, @vsubject)"; // sql문
+
+            sqlC3 = new SqlCommand(s, sql);
+            sqlA2 = new SqlDataAdapter();
+            sqlA2.InsertCommand = sqlC3; // Insert문 명령으로 sqlC3을 넘겨줌
+
+            sqlC3.Parameters.Add("@mcode", SqlDbType.Int, 4);
+            sqlC3.Parameters.Add("@vcode", SqlDbType.VarChar, 14);
+            sqlC3.Parameters.Add("@lent_date", SqlDbType.VarChar, 10);
+            sqlC3.Parameters.Add("@return_date1", SqlDbType.VarChar, 10);
+            sqlC3.Parameters.Add("@return_date2", SqlDbType.VarChar, 10);
+            sqlC3.Parameters.Add("@lent_pay", SqlDbType.Int, 4);
+            sqlC3.Parameters.Add("@delay_pay1", SqlDbType.Int, 4);
+            sqlC3.Parameters.Add("@delay_pay2", SqlDbType.Int, 4);
+            sqlC3.Parameters.Add("@vsubject", SqlDbType.VarChar, 100);
+
+            DataRow LRow;
+            LRow = DS2.Tables["lent"].NewRow();
+
+            LRow["mcode"] = TBcode.Text;
+            LRow["vcode"] = TBvcode.Text;
+            LRow["lent_date"] = dateTimePicker1.Value;
+            LRow["vsubject"] = TBvtitle.Text;
+            LRow["return_date1"] = TBoutdate.Text;
+            LRow["return_date2"] = "대여중";
+            LRow["lent_pay"] = TBlentpay.Text;
+            LRow["delay_pay1"] = TBdelaypay.Text;
+            LRow["delay_pay2"] = 0;
+
+            DS2.Tables["lent"].Rows.Add(LRow);
+
+            sqlC3.Parameters["@mcode"].Value = LRow["mcode"];
+            sqlC3.Parameters["@vcode"].Value = LRow["vcode"];
+            sqlC3.Parameters["@vsubject"].Value = LRow["vsubject"];
+            sqlC3.Parameters["@lent_date"].Value = LRow["lent_date"];
+            sqlC3.Parameters["@return_date1"].Value = LRow["return_date1"];
+            sqlC3.Parameters["@return_date2"].Value = LRow["return_date2"];
+            sqlC3.Parameters["@lent_pay"].Value = LRow["lent_pay"];
+            sqlC3.Parameters["@delay_pay1"].Value = LRow["delay_pay1"];
+            sqlC3.Parameters["@delay_pay2"].Value = LRow["delay_pay2"];
+
+            i += Convert.ToInt32(LRow["lent_pay"]);
+            lbl_alllentpay.Text = i + " 원";
+            lbl_alllentcount.Text = DS2.Tables["lent"].Rows.Count + " 개";
+
+            sqlA2.Update(DS2.Tables["lent"]); // DB 업데이트
+
+            TBvcode.Text = "";
+            TBvtitle.Text = "";
+            TBlentpay.Text = "";
+            TBdelaypay.Text = "";
+            TBoutdate.Text = "0000-00-00";
+            bt_save.Enabled = false;
         }
 
         private void Bt_vreturn_Click(object sender, EventArgs e)
         {
-            
+            SqlCommand sqlC3;
+            string s = "UPDATE lent SET mcode = @mcode, return_date2 = @return_date2, " +
+                "delay_pay2 = @delay_pay2 WHERE lentcode = @lentcode";
+            sqlC3 = new SqlCommand(s, sql);
+            sqlA2.UpdateCommand = sqlC3; // update문 명령으로 sqlC3을 넘겨줌
+
+            sqlC3.Parameters.Add("@mcode", SqlDbType.Int, 4);
+            sqlC3.Parameters.Add("@return_date2", SqlDbType.VarChar, 10);
+            sqlC3.Parameters.Add("@delay_pay2", SqlDbType.Int, 4);
+            sqlC3.Parameters.Add("@lentcode", SqlDbType.Int, 4);
+
+            try
+            {
+                DataRow LRow = DS2.Tables["lent"].Rows[dgVideo.CurrentCell.RowIndex];
+
+                TimeSpan DT = DateTime.Today.Date - Convert.ToDateTime(LRow["return_date1"]);
+                int k = Convert.ToInt32(DT.TotalDays);
+
+                LRow.BeginEdit(); 
+                LRow["return_date2"] = DateTime.Today.Date;
+                if (k < 1)
+                {
+                    LRow["mcode"] = "-" + LRow["mcode"];
+                }
+                else
+                {
+                    LRow["delay_pay2"] = Convert.ToInt32(LRow["delay_pay1"]) * k;
+
+                    DialogResult msg;
+                    msg = MessageBox.Show(LRow["delay_pay2"] + " 원의 연체료가 있습니다." +
+                        "삭제하겠습니까?", "대여 비디오 반납", MessageBoxButtons.YesNoCancel,
+                        MessageBoxIcon.Warning);
+                    if (msg == DialogResult.Yes)
+                    {
+                        LRow["mcode"] = "-" + LRow["mcode"];
+                    }
+                    else if (msg == DialogResult.No)
+                    {
+                        j += Convert.ToInt32(LRow["delay_pay2"]);
+                        lbl_alldelaypay.Text = j + " 원";
+                    }
+                    else
+                    {
+                        LRow.RejectChanges();
+                    }
+                }
+                LRow.EndEdit();
+                sqlC3.Parameters["@lentcode"].Value = LRow["lentcode"];
+                sqlC3.Parameters["@mcode"].Value = LRow["mcode"];
+                sqlC3.Parameters["@return_date2"].Value = LRow["return_date2"].ToString();
+                sqlC3.Parameters["@delay_pay2"].Value = LRow["delay_pay2"];
+                sqlA2.Update(DS2.Tables["lent"]); // DB 업데이트
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            MemberSelect();
         }
 
         private void LentMg_FormClosing(object sender, FormClosingEventArgs e)
