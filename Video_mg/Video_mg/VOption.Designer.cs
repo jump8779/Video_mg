f8fb873b3e224fa7703e383a4c0174a6110af84d
jump8779 <jump154159@gmail.com>
2019-09-18 00:02:52 +0900

    VOption폼 디자인 작성
    GroupBox
    + 신 프로 그룹 : GB_newpro
    + 구 프로 그룹 : GB_oldpro
    
    Button
    + 등록 버튼 : bt_registe
    + 수정 버튼 : bt_edit
    + 나가기 버튼 : bt_exit
    
    TextBox
    GB_newpro
    + 전환기간 입력 : TBnew_cdate
    + 대여기간 입력 : TBnew_lentdate
    + 대여료 입력 : TBnew_lentpay
    + 연체료 입력 : TBnew_delaypay
    GB_oldpro
    + 대여기간 입력 : TBold_lentdate
    + 대여료 입력 : TBold_lentpay
    + 연체료 입력 : TBold_delaypay

--- Video_mg/Video_mg/VOption.Designer.cs
+++ Video_mg/Video_mg/VOption.Designer.cs
@@ -28,19 +28,327 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.GB_newpro = new System.Windows.Forms.GroupBox();
+            this.GB_oldpro = new System.Windows.Forms.GroupBox();
+            this.bt_registe = new System.Windows.Forms.Button();
+            this.bt_edit = new System.Windows.Forms.Button();
+            this.bt_exit = new System.Windows.Forms.Button();
+            this.lblNchangedate = new System.Windows.Forms.Label();
+            this.lblNlentdate = new System.Windows.Forms.Label();
+            this.lblNdate1 = new System.Windows.Forms.Label();
+            this.lblNdate2 = new System.Windows.Forms.Label();
+            this.lblNlentpay = new System.Windows.Forms.Label();
+            this.lblNdelaypay = new System.Windows.Forms.Label();
+            this.lblNpay1 = new System.Windows.Forms.Label();
+            this.lblNpay2 = new System.Windows.Forms.Label();
+            this.lblOlentpay = new System.Windows.Forms.Label();
+            this.lblOdelaypay = new System.Windows.Forms.Label();
+            this.lblOpay1 = new System.Windows.Forms.Label();
+            this.lblOpay2 = new System.Windows.Forms.Label();
+            this.lblOlentdate = new System.Windows.Forms.Label();
+            this.lblOdate = new System.Windows.Forms.Label();
+            this.TBnew_cdate = new System.Windows.Forms.TextBox();
+            this.TBnew_lentdate = new System.Windows.Forms.TextBox();
+            this.TBnew_delaypay = new System.Windows.Forms.TextBox();
+            this.TBnew_lentpay = new System.Windows.Forms.TextBox();
+            this.TBold_lentpay = new System.Windows.Forms.TextBox();
+            this.TBold_delaypay = new System.Windows.Forms.TextBox();
+            this.TBold_lentdate = new System.Windows.Forms.TextBox();
+            this.GB_newpro.SuspendLayout();
+            this.GB_oldpro.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // GB_newpro
+            // 
+            this.GB_newpro.Controls.Add(this.TBnew_lentpay);
+            this.GB_newpro.Controls.Add(this.TBnew_delaypay);
+            this.GB_newpro.Controls.Add(this.TBnew_lentdate);
+            this.GB_newpro.Controls.Add(this.TBnew_cdate);
+            this.GB_newpro.Controls.Add(this.lblNpay2);
+            this.GB_newpro.Controls.Add(this.lblNpay1);
+            this.GB_newpro.Controls.Add(this.lblNdelaypay);
+            this.GB_newpro.Controls.Add(this.lblNlentpay);
+            this.GB_newpro.Controls.Add(this.lblNdate2);
+            this.GB_newpro.Controls.Add(this.lblNdate1);
+            this.GB_newpro.Controls.Add(this.lblNlentdate);
+            this.GB_newpro.Controls.Add(this.lblNchangedate);
+            this.GB_newpro.Location = new System.Drawing.Point(13, 13);
+            this.GB_newpro.Name = "GB_newpro";
+            this.GB_newpro.Size = new System.Drawing.Size(526, 100);
+            this.GB_newpro.TabIndex = 0;
+            this.GB_newpro.TabStop = false;
+            this.GB_newpro.Text = "신 프로";
+            // 
+            // GB_oldpro
+            // 
+            this.GB_oldpro.Controls.Add(this.TBold_lentdate);
+            this.GB_oldpro.Controls.Add(this.TBold_delaypay);
+            this.GB_oldpro.Controls.Add(this.TBold_lentpay);
+            this.GB_oldpro.Controls.Add(this.lblOpay2);
+            this.GB_oldpro.Controls.Add(this.lblOlentpay);
+            this.GB_oldpro.Controls.Add(this.lblOpay1);
+            this.GB_oldpro.Controls.Add(this.lblOdelaypay);
+            this.GB_oldpro.Controls.Add(this.lblOdate);
+            this.GB_oldpro.Controls.Add(this.lblOlentdate);
+            this.GB_oldpro.Location = new System.Drawing.Point(13, 122);
+            this.GB_oldpro.Name = "GB_oldpro";
+            this.GB_oldpro.Size = new System.Drawing.Size(526, 100);
+            this.GB_oldpro.TabIndex = 1;
+            this.GB_oldpro.TabStop = false;
+            this.GB_oldpro.Text = "구 프로";
+            // 
+            // bt_registe
+            // 
+            this.bt_registe.Location = new System.Drawing.Point(13, 234);
+            this.bt_registe.Name = "bt_registe";
+            this.bt_registe.Size = new System.Drawing.Size(115, 39);
+            this.bt_registe.TabIndex = 2;
+            this.bt_registe.Text = "등록";
+            this.bt_registe.UseVisualStyleBackColor = true;
+            // 
+            // bt_edit
+            // 
+            this.bt_edit.Location = new System.Drawing.Point(134, 234);
+            this.bt_edit.Name = "bt_edit";
+            this.bt_edit.Size = new System.Drawing.Size(115, 39);
+            this.bt_edit.TabIndex = 2;
+            this.bt_edit.Text = "수정";
+            this.bt_edit.UseVisualStyleBackColor = true;
+            // 
+            // bt_exit
+            // 
+            this.bt_exit.Location = new System.Drawing.Point(424, 234);
+            this.bt_exit.Name = "bt_exit";
+            this.bt_exit.Size = new System.Drawing.Size(115, 39);
+            this.bt_exit.TabIndex = 2;
+            this.bt_exit.Text = "나가기";
+            this.bt_exit.UseVisualStyleBackColor = true;
+            // 
+            // lblNchangedate
+            // 
+            this.lblNchangedate.AutoSize = true;
+            this.lblNchangedate.Location = new System.Drawing.Point(7, 31);
+            this.lblNchangedate.Name = "lblNchangedate";
+            this.lblNchangedate.Size = new System.Drawing.Size(82, 15);
+            this.lblNchangedate.TabIndex = 0;
+            this.lblNchangedate.Text = "전환 기간 :";
+            // 
+            // lblNlentdate
+            // 
+            this.lblNlentdate.AutoSize = true;
+            this.lblNlentdate.Location = new System.Drawing.Point(7, 65);
+            this.lblNlentdate.Name = "lblNlentdate";
+            this.lblNlentdate.Size = new System.Drawing.Size(82, 15);
+            this.lblNlentdate.TabIndex = 1;
+            this.lblNlentdate.Text = "대여 기간 :";
+            // 
+            // lblNdate1
+            // 
+            this.lblNdate1.AutoSize = true;
+            this.lblNdate1.Location = new System.Drawing.Point(211, 31);
+            this.lblNdate1.Name = "lblNdate1";
+            this.lblNdate1.Size = new System.Drawing.Size(57, 15);
+            this.lblNdate1.TabIndex = 2;
+            this.lblNdate1.Text = "일 이내";
+            // 
+            // lblNdate2
+            // 
+            this.lblNdate2.AutoSize = true;
+            this.lblNdate2.Location = new System.Drawing.Point(211, 65);
+            this.lblNdate2.Name = "lblNdate2";
+            this.lblNdate2.Size = new System.Drawing.Size(22, 15);
+            this.lblNdate2.TabIndex = 3;
+            this.lblNdate2.Text = "일";
+            // 
+            // lblNlentpay
+            // 
+            this.lblNlentpay.AutoSize = true;
+            this.lblNlentpay.Location = new System.Drawing.Point(303, 31);
+            this.lblNlentpay.Name = "lblNlentpay";
+            this.lblNlentpay.Size = new System.Drawing.Size(62, 15);
+            this.lblNlentpay.TabIndex = 4;
+            this.lblNlentpay.Text = "대여료 :";
+            // 
+            // lblNdelaypay
+            // 
+            this.lblNdelaypay.AutoSize = true;
+            this.lblNdelaypay.Location = new System.Drawing.Point(303, 65);
+            this.lblNdelaypay.Name = "lblNdelaypay";
+            this.lblNdelaypay.Size = new System.Drawing.Size(62, 15);
+            this.lblNdelaypay.TabIndex = 5;
+            this.lblNdelaypay.Text = "연체료 :";
+            // 
+            // lblNpay1
+            // 
+            this.lblNpay1.AutoSize = true;
+            this.lblNpay1.Location = new System.Drawing.Point(488, 31);
+            this.lblNpay1.Name = "lblNpay1";
+            this.lblNpay1.Size = new System.Drawing.Size(22, 15);
+            this.lblNpay1.TabIndex = 6;
+            this.lblNpay1.Text = "원";
+            // 
+            // lblNpay2
+            // 
+            this.lblNpay2.AutoSize = true;
+            this.lblNpay2.Location = new System.Drawing.Point(488, 65);
+            this.lblNpay2.Name = "lblNpay2";
+            this.lblNpay2.Size = new System.Drawing.Size(22, 15);
+            this.lblNpay2.TabIndex = 7;
+            this.lblNpay2.Text = "원";
+            // 
+            // lblOlentpay
+            // 
+            this.lblOlentpay.AutoSize = true;
+            this.lblOlentpay.Location = new System.Drawing.Point(303, 30);
+            this.lblOlentpay.Name = "lblOlentpay";
+            this.lblOlentpay.Size = new System.Drawing.Size(62, 15);
+            this.lblOlentpay.TabIndex = 4;
+            this.lblOlentpay.Text = "대여료 :";
+            // 
+            // lblOdelaypay
+            // 
+            this.lblOdelaypay.AutoSize = true;
+            this.lblOdelaypay.Location = new System.Drawing.Point(303, 64);
+            this.lblOdelaypay.Name = "lblOdelaypay";
+            this.lblOdelaypay.Size = new System.Drawing.Size(62, 15);
+            this.lblOdelaypay.TabIndex = 5;
+            this.lblOdelaypay.Text = "연체료 :";
+            // 
+            // lblOpay1
+            // 
+            this.lblOpay1.AutoSize = true;
+            this.lblOpay1.Location = new System.Drawing.Point(488, 30);
+            this.lblOpay1.Name = "lblOpay1";
+            this.lblOpay1.Size = new System.Drawing.Size(22, 15);
+            this.lblOpay1.TabIndex = 6;
+            this.lblOpay1.Text = "원";
+            // 
+            // lblOpay2
+            // 
+            this.lblOpay2.AutoSize = true;
+            this.lblOpay2.Location = new System.Drawing.Point(488, 64);
+            this.lblOpay2.Name = "lblOpay2";
+            this.lblOpay2.Size = new System.Drawing.Size(22, 15);
+            this.lblOpay2.TabIndex = 7;
+            this.lblOpay2.Text = "원";
+            // 
+            // lblOlentdate
+            // 
+            this.lblOlentdate.AutoSize = true;
+            this.lblOlentdate.Location = new System.Drawing.Point(7, 48);
+            this.lblOlentdate.Name = "lblOlentdate";
+            this.lblOlentdate.Size = new System.Drawing.Size(82, 15);
+            this.lblOlentdate.TabIndex = 1;
+            this.lblOlentdate.Text = "대여 기간 :";
+            // 
+            // lblOdate
+            // 
+            this.lblOdate.AutoSize = true;
+            this.lblOdate.Location = new System.Drawing.Point(211, 48);
+            this.lblOdate.Name = "lblOdate";
+            this.lblOdate.Size = new System.Drawing.Size(22, 15);
+            this.lblOdate.TabIndex = 3;
+            this.lblOdate.Text = "일";
+            // 
+            // TBnew_cdate
+            // 
+            this.TBnew_cdate.Location = new System.Drawing.Point(91, 27);
+            this.TBnew_cdate.Name = "TBnew_cdate";
+            this.TBnew_cdate.Size = new System.Drawing.Size(114, 25);
+            this.TBnew_cdate.TabIndex = 8;
+            // 
+            // TBnew_lentdate
+            // 
+            this.TBnew_lentdate.Location = new System.Drawing.Point(91, 61);
+            this.TBnew_lentdate.Name = "TBnew_lentdate";
+            this.TBnew_lentdate.Size = new System.Drawing.Size(114, 25);
+            this.TBnew_lentdate.TabIndex = 8;
+            // 
+            // TBnew_delaypay
+            // 
+            this.TBnew_delaypay.Location = new System.Drawing.Point(367, 61);
+            this.TBnew_delaypay.Name = "TBnew_delaypay";
+            this.TBnew_delaypay.Size = new System.Drawing.Size(114, 25);
+            this.TBnew_delaypay.TabIndex = 8;
+            // 
+            // TBnew_lentpay
+            // 
+            this.TBnew_lentpay.Location = new System.Drawing.Point(367, 27);
+            this.TBnew_lentpay.Name = "TBnew_lentpay";
+            this.TBnew_lentpay.Size = new System.Drawing.Size(114, 25);
+            this.TBnew_lentpay.TabIndex = 8;
+            // 
+            // TBold_lentpay
+            // 
+            this.TBold_lentpay.Location = new System.Drawing.Point(367, 26);
+            this.TBold_lentpay.Name = "TBold_lentpay";
+            this.TBold_lentpay.Size = new System.Drawing.Size(114, 25);
+            this.TBold_lentpay.TabIndex = 8;
+            // 
+            // TBold_delaypay
+            // 
+            this.TBold_delaypay.Location = new System.Drawing.Point(367, 60);
+            this.TBold_delaypay.Name = "TBold_delaypay";
+            this.TBold_delaypay.Size = new System.Drawing.Size(114, 25);
+            this.TBold_delaypay.TabIndex = 8;
+            // 
+            // TBold_lentdate
+            // 
+            this.TBold_lentdate.Location = new System.Drawing.Point(91, 44);
+            this.TBold_lentdate.Name = "TBold_lentdate";
+            this.TBold_lentdate.Size = new System.Drawing.Size(114, 25);
+            this.TBold_lentdate.TabIndex = 8;
             // 
             // VOption
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(551, 285);
+            this.Controls.Add(this.bt_exit);
+            this.Controls.Add(this.bt_edit);
+            this.Controls.Add(this.bt_registe);
+            this.Controls.Add(this.GB_oldpro);
+            this.Controls.Add(this.GB_newpro);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(569, 332);
+            this.MinimumSize = new System.Drawing.Size(569, 332);
             this.Name = "VOption";
-            this.Text = "VOption";
+            this.Text = "비디오 대여 설정";
+            this.GB_newpro.ResumeLayout(false);
+            this.GB_newpro.PerformLayout();
+            this.GB_oldpro.ResumeLayout(false);
+            this.GB_oldpro.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox GB_newpro;
+        private System.Windows.Forms.TextBox TBnew_lentpay;
+        private System.Windows.Forms.TextBox TBnew_delaypay;
+        private System.Windows.Forms.TextBox TBnew_lentdate;
+        private System.Windows.Forms.TextBox TBnew_cdate;
+        private System.Windows.Forms.Label lblNpay2;
+        private System.Windows.Forms.Label lblNpay1;
+        private System.Windows.Forms.Label lblNdelaypay;
+        private System.Windows.Forms.Label lblNlentpay;
+        private System.Windows.Forms.Label lblNdate2;
+        private System.Windows.Forms.Label lblNdate1;
+        private System.Windows.Forms.Label lblNlentdate;
+        private System.Windows.Forms.Label lblNchangedate;
+        private System.Windows.Forms.GroupBox GB_oldpro;
+        private System.Windows.Forms.TextBox TBold_lentdate;
+        private System.Windows.Forms.TextBox TBold_delaypay;
+        private System.Windows.Forms.TextBox TBold_lentpay;
+        private System.Windows.Forms.Label lblOpay2;
+        private System.Windows.Forms.Label lblOlentpay;
+        private System.Windows.Forms.Label lblOpay1;
+        private System.Windows.Forms.Label lblOdelaypay;
+        private System.Windows.Forms.Label lblOdate;
+        private System.Windows.Forms.Label lblOlentdate;
+        private System.Windows.Forms.Button bt_registe;
+        private System.Windows.Forms.Button bt_edit;
+        private System.Windows.Forms.Button bt_exit;
     }
 }
