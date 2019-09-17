475886a74e66a4c5bdd8ea8d36d0772aded72702
jump8779 <jump154159@gmail.com>
2019-09-17 19:47:58 +0900

    MemberMg 폼 디자인 작성
    GroupBox
    + 고객정보 그룹 : GB_memberinfo
    
    Label
    + 현재 고객 수 출력 : lbl_allmcount
    
    TextBox
    + 고객코드 입력 : TBmcode
    + 고객명 입력 : TBiname
    + 주민번호 입력 : TBjumin
    + 고객신분 입력 : TBsinbun
    + 성별 입력 : TBsex
    + 전화번호 입력 : TBphone
    + 휴대폰 입력 : TBpcs
    + 우편번호 입력 : TBzip
    + 주소 입력 : TBaddress
    
    Button
    + 추가 버튼 : bt_add
    + 저장 버튼 : bt_save
    + 삭제 버튼 : bt_delete
    + 취소 버튼 : bt_cancel
    + 카드관리 버튼 : bt_Card
    + 나가기 버튼 : bt_exit
    
    DataGridView
    + 고객 리스트 데이터 그리드 뷰 : dgMember

---
+++ Video_mg/Video_mg/MemberMg.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace Video_mg
+{
+    partial class MemberMg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GB_memberinfo = new System.Windows.Forms.GroupBox();
+            this.TBaddress = new System.Windows.Forms.TextBox();
+            this.TBzip = new System.Windows.Forms.TextBox();
+            this.TBpcs = new System.Windows.Forms.TextBox();
+            this.TBphone = new System.Windows.Forms.TextBox();
+            this.TBsex = new System.Windows.Forms.TextBox();
+            this.TBsinbun = new System.Windows.Forms.TextBox();
+            this.TBjumin = new System.Windows.Forms.TextBox();
+            this.TBiname = new System.Windows.Forms.TextBox();
+            this.TBmcode = new System.Windows.Forms.TextBox();
+            this.lbladdress = new System.Windows.Forms.Label();
+            this.lblzip = new System.Windows.Forms.Label();
+            this.lblpcs = new System.Windows.Forms.Label();
+            this.lblphone = new System.Windows.Forms.Label();
+            this.lblsex = new System.Windows.Forms.Label();
+            this.lblsinbun = new System.Windows.Forms.Label();
+            this.lbljumin = new System.Windows.Forms.Label();
+            this.lbliname = new System.Windows.Forms.Label();
+            this.lblmcode = new System.Windows.Forms.Label();
+            this.bt_add = new System.Windows.Forms.Button();
+            this.bt_save = new System.Windows.Forms.Button();
+            this.bt_delete = new System.Windows.Forms.Button();
+            this.bt_cancel = new System.Windows.Forms.Button();
+            this.bt_exit = new System.Windows.Forms.Button();
+            this.bt_Card = new System.Windows.Forms.Button();
+            this.lblallmember = new System.Windows.Forms.Label();
+            this.lbl_allmcount = new System.Windows.Forms.Label();
+            this.dgMember = new System.Windows.Forms.DataGridView();
+            this.GB_memberinfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMember)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GB_memberinfo
+            // 
+            this.GB_memberinfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GB_memberinfo.Controls.Add(this.TBaddress);
+            this.GB_memberinfo.Controls.Add(this.TBzip);
+            this.GB_memberinfo.Controls.Add(this.TBpcs);
+            this.GB_memberinfo.Controls.Add(this.TBphone);
+            this.GB_memberinfo.Controls.Add(this.TBsex);
+            this.GB_memberinfo.Controls.Add(this.TBsinbun);
+            this.GB_memberinfo.Controls.Add(this.TBjumin);
+            this.GB_memberinfo.Controls.Add(this.TBiname);
+            this.GB_memberinfo.Controls.Add(this.TBmcode);
+            this.GB_memberinfo.Controls.Add(this.lbladdress);
+            this.GB_memberinfo.Controls.Add(this.lblzip);
+            this.GB_memberinfo.Controls.Add(this.lblpcs);
+            this.GB_memberinfo.Controls.Add(this.lblphone);
+            this.GB_memberinfo.Controls.Add(this.lblsex);
+            this.GB_memberinfo.Controls.Add(this.lblsinbun);
+            this.GB_memberinfo.Controls.Add(this.lbljumin);
+            this.GB_memberinfo.Controls.Add(this.lbliname);
+            this.GB_memberinfo.Controls.Add(this.lblmcode);
+            this.GB_memberinfo.Location = new System.Drawing.Point(13, 13);
+            this.GB_memberinfo.Name = "GB_memberinfo";
+            this.GB_memberinfo.Size = new System.Drawing.Size(866, 270);
+            this.GB_memberinfo.TabIndex = 0;
+            this.GB_memberinfo.TabStop = false;
+            this.GB_memberinfo.Text = "고객 정보";
+            // 
+            // TBaddress
+            // 
+            this.TBaddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBaddress.Location = new System.Drawing.Point(99, 227);
+            this.TBaddress.Name = "TBaddress";
+            this.TBaddress.Size = new System.Drawing.Size(740, 25);
+            this.TBaddress.TabIndex = 17;
+            // 
+            // TBzip
+            // 
+            this.TBzip.Location = new System.Drawing.Point(99, 187);
+            this.TBzip.Name = "TBzip";
+            this.TBzip.Size = new System.Drawing.Size(289, 25);
+            this.TBzip.TabIndex = 16;
+            // 
+            // TBpcs
+            // 
+            this.TBpcs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBpcs.Location = new System.Drawing.Point(524, 146);
+            this.TBpcs.Name = "TBpcs";
+            this.TBpcs.Size = new System.Drawing.Size(315, 25);
+            this.TBpcs.TabIndex = 15;
+            // 
+            // TBphone
+            // 
+            this.TBphone.Location = new System.Drawing.Point(99, 146);
+            this.TBphone.Name = "TBphone";
+            this.TBphone.Size = new System.Drawing.Size(289, 25);
+            this.TBphone.TabIndex = 14;
+            // 
+            // TBsex
+            // 
+            this.TBsex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBsex.Location = new System.Drawing.Point(524, 107);
+            this.TBsex.Name = "TBsex";
+            this.TBsex.Size = new System.Drawing.Size(315, 25);
+            this.TBsex.TabIndex = 13;
+            // 
+            // TBsinbun
+            // 
+            this.TBsinbun.Location = new System.Drawing.Point(99, 106);
+            this.TBsinbun.Name = "TBsinbun";
+            this.TBsinbun.Size = new System.Drawing.Size(289, 25);
+            this.TBsinbun.TabIndex = 12;
+            // 
+            // TBjumin
+            // 
+            this.TBjumin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBjumin.Location = new System.Drawing.Point(524, 66);
+            this.TBjumin.Name = "TBjumin";
+            this.TBjumin.Size = new System.Drawing.Size(315, 25);
+            this.TBjumin.TabIndex = 11;
+            // 
+            // TBiname
+            // 
+            this.TBiname.Location = new System.Drawing.Point(99, 66);
+            this.TBiname.Name = "TBiname";
+            this.TBiname.Size = new System.Drawing.Size(289, 25);
+            this.TBiname.TabIndex = 10;
+            // 
+            // TBmcode
+            // 
+            this.TBmcode.Location = new System.Drawing.Point(99, 30);
+            this.TBmcode.Name = "TBmcode";
+            this.TBmcode.Size = new System.Drawing.Size(156, 25);
+            this.TBmcode.TabIndex = 9;
+            // 
+            // lbladdress
+            // 
+            this.lbladdress.AutoSize = true;
+            this.lbladdress.Location = new System.Drawing.Point(50, 231);
+            this.lbladdress.Name = "lbladdress";
+            this.lbladdress.Size = new System.Drawing.Size(47, 15);
+            this.lbladdress.TabIndex = 8;
+            this.lbladdress.Text = "주소 :";
+            // 
+            // lblzip
+            // 
+            this.lblzip.AutoSize = true;
+            this.lblzip.Location = new System.Drawing.Point(20, 191);
+            this.lblzip.Name = "lblzip";
+            this.lblzip.Size = new System.Drawing.Size(77, 15);
+            this.lblzip.TabIndex = 7;
+            this.lblzip.Text = "우편번호 :";
+            // 
+            // lblpcs
+            // 
+            this.lblpcs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblpcs.AutoSize = true;
+            this.lblpcs.Location = new System.Drawing.Point(460, 150);
+            this.lblpcs.Name = "lblpcs";
+            this.lblpcs.Size = new System.Drawing.Size(62, 15);
+            this.lblpcs.TabIndex = 6;
+            this.lblpcs.Text = "휴대폰 :";
+            // 
+            // lblphone
+            // 
+            this.lblphone.AutoSize = true;
+            this.lblphone.Location = new System.Drawing.Point(20, 150);
+            this.lblphone.Name = "lblphone";
+            this.lblphone.Size = new System.Drawing.Size(77, 15);
+            this.lblphone.TabIndex = 5;
+            this.lblphone.Text = "전화번호 :";
+            // 
+            // lblsex
+            // 
+            this.lblsex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblsex.AutoSize = true;
+            this.lblsex.Location = new System.Drawing.Point(475, 110);
+            this.lblsex.Name = "lblsex";
+            this.lblsex.Size = new System.Drawing.Size(47, 15);
+            this.lblsex.TabIndex = 4;
+            this.lblsex.Text = "성별 :";
+            // 
+            // lblsinbun
+            // 
+            this.lblsinbun.AutoSize = true;
+            this.lblsinbun.Location = new System.Drawing.Point(20, 110);
+            this.lblsinbun.Name = "lblsinbun";
+            this.lblsinbun.Size = new System.Drawing.Size(77, 15);
+            this.lblsinbun.TabIndex = 3;
+            this.lblsinbun.Text = "고객신분 :";
+            // 
+            // lbljumin
+            // 
+            this.lbljumin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbljumin.AutoSize = true;
+            this.lbljumin.Location = new System.Drawing.Point(415, 70);
+            this.lbljumin.Name = "lbljumin";
+            this.lbljumin.Size = new System.Drawing.Size(107, 15);
+            this.lbljumin.TabIndex = 2;
+            this.lbljumin.Text = "주민등록번호 :";
+            // 
+            // lbliname
+            // 
+            this.lbliname.AutoSize = true;
+            this.lbliname.Location = new System.Drawing.Point(35, 70);
+            this.lbliname.Name = "lbliname";
+            this.lbliname.Size = new System.Drawing.Size(62, 15);
+            this.lbliname.TabIndex = 1;
+            this.lbliname.Text = "고객명 :";
+            // 
+            // lblmcode
+            // 
+            this.lblmcode.AutoSize = true;
+            this.lblmcode.Location = new System.Drawing.Point(20, 34);
+            this.lblmcode.Name = "lblmcode";
+            this.lblmcode.Size = new System.Drawing.Size(77, 15);
+            this.lblmcode.TabIndex = 0;
+            this.lblmcode.Text = "고객번호 :";
+            // 
+            // bt_add
+            // 
+            this.bt_add.Location = new System.Drawing.Point(13, 289);
+            this.bt_add.Name = "bt_add";
+            this.bt_add.Size = new System.Drawing.Size(101, 36);
+            this.bt_add.TabIndex = 1;
+            this.bt_add.Text = "추가";
+            this.bt_add.UseVisualStyleBackColor = true;
+            // 
+            // bt_save
+            // 
+            this.bt_save.Location = new System.Drawing.Point(120, 289);
+            this.bt_save.Name = "bt_save";
+            this.bt_save.Size = new System.Drawing.Size(101, 36);
+            this.bt_save.TabIndex = 1;
+            this.bt_save.Text = "저장";
+            this.bt_save.UseVisualStyleBackColor = true;
+            // 
+            // bt_delete
+            // 
+            this.bt_delete.Location = new System.Drawing.Point(227, 289);
+            this.bt_delete.Name = "bt_delete";
+            this.bt_delete.Size = new System.Drawing.Size(101, 36);
+            this.bt_delete.TabIndex = 1;
+            this.bt_delete.Text = "삭제";
+            this.bt_delete.UseVisualStyleBackColor = true;
+            // 
+            // bt_cancel
+            // 
+            this.bt_cancel.Location = new System.Drawing.Point(334, 289);
+            this.bt_cancel.Name = "bt_cancel";
+            this.bt_cancel.Size = new System.Drawing.Size(101, 36);
+            this.bt_cancel.TabIndex = 1;
+            this.bt_cancel.Text = "취소";
+            this.bt_cancel.UseVisualStyleBackColor = true;
+            // 
+            // bt_exit
+            // 
+            this.bt_exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_exit.Location = new System.Drawing.Point(778, 289);
+            this.bt_exit.Name = "bt_exit";
+            this.bt_exit.Size = new System.Drawing.Size(101, 36);
+            this.bt_exit.TabIndex = 1;
+            this.bt_exit.Text = "나가기";
+            this.bt_exit.UseVisualStyleBackColor = true;
+            // 
+            // bt_Card
+            // 
+            this.bt_Card.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_Card.Location = new System.Drawing.Point(671, 289);
+            this.bt_Card.Name = "bt_Card";
+            this.bt_Card.Size = new System.Drawing.Size(101, 36);
+            this.bt_Card.TabIndex = 1;
+            this.bt_Card.Text = "카드 관리";
+            this.bt_Card.UseVisualStyleBackColor = true;
+            // 
+            // lblallmember
+            // 
+            this.lblallmember.AutoSize = true;
+            this.lblallmember.Location = new System.Drawing.Point(21, 340);
+            this.lblallmember.Name = "lblallmember";
+            this.lblallmember.Size = new System.Drawing.Size(102, 15);
+            this.lblallmember.TabIndex = 2;
+            this.lblallmember.Text = "현재 고객 수 :";
+            // 
+            // lbl_allmcount
+            // 
+            this.lbl_allmcount.BackColor = System.Drawing.Color.Black;
+            this.lbl_allmcount.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbl_allmcount.ForeColor = System.Drawing.Color.White;
+            this.lbl_allmcount.Location = new System.Drawing.Point(129, 334);
+            this.lbl_allmcount.Name = "lbl_allmcount";
+            this.lbl_allmcount.Size = new System.Drawing.Size(60, 28);
+            this.lbl_allmcount.TabIndex = 3;
+            this.lbl_allmcount.Text = "0 명";
+            this.lbl_allmcount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgMember
+            // 
+            this.dgMember.AllowUserToAddRows = false;
+            this.dgMember.AllowUserToDeleteRows = false;
+            this.dgMember.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgMember.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMember.Location = new System.Drawing.Point(13, 365);
+            this.dgMember.Name = "dgMember";
+            this.dgMember.ReadOnly = true;
+            this.dgMember.RowHeadersWidth = 51;
+            this.dgMember.RowTemplate.Height = 27;
+            this.dgMember.Size = new System.Drawing.Size(866, 201);
+            this.dgMember.TabIndex = 4;
+            // 
+            // MemberMg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 578);
+            this.Controls.Add(this.dgMember);
+            this.Controls.Add(this.lbl_allmcount);
+            this.Controls.Add(this.lblallmember);
+            this.Controls.Add(this.bt_Card);
+            this.Controls.Add(this.bt_exit);
+            this.Controls.Add(this.bt_cancel);
+            this.Controls.Add(this.bt_delete);
+            this.Controls.Add(this.bt_save);
+            this.Controls.Add(this.bt_add);
+            this.Controls.Add(this.GB_memberinfo);
+            this.MinimumSize = new System.Drawing.Size(909, 625);
+            this.Name = "MemberMg";
+            this.Text = "고객 정보";
+            this.GB_memberinfo.ResumeLayout(false);
+            this.GB_memberinfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMember)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GB_memberinfo;
+        private System.Windows.Forms.TextBox TBaddress;
+        private System.Windows.Forms.TextBox TBzip;
+        private System.Windows.Forms.TextBox TBpcs;
+        private System.Windows.Forms.TextBox TBphone;
+        private System.Windows.Forms.TextBox TBsex;
+        private System.Windows.Forms.TextBox TBsinbun;
+        private System.Windows.Forms.TextBox TBjumin;
+        private System.Windows.Forms.TextBox TBiname;
+        private System.Windows.Forms.TextBox TBmcode;
+        private System.Windows.Forms.Label lbladdress;
+        private System.Windows.Forms.Label lblzip;
+        private System.Windows.Forms.Label lblpcs;
+        private System.Windows.Forms.Label lblphone;
+        private System.Windows.Forms.Label lblsex;
+        private System.Windows.Forms.Label lblsinbun;
+        private System.Windows.Forms.Label lbljumin;
+        private System.Windows.Forms.Label lbliname;
+        private System.Windows.Forms.Label lblmcode;
+        private System.Windows.Forms.Button bt_add;
+        private System.Windows.Forms.Button bt_save;
+        private System.Windows.Forms.Button bt_delete;
+        private System.Windows.Forms.Button bt_cancel;
+        private System.Windows.Forms.Button bt_exit;
+        private System.Windows.Forms.Button bt_Card;
+        private System.Windows.Forms.Label lblallmember;
+        private System.Windows.Forms.Label lbl_allmcount;
+        private System.Windows.Forms.DataGridView dgMember;
+    }
+}
